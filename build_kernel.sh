21e68e76857fa4f7fd0b49d9d13d8a2d31fb4f56
hodayfa000h <127386194+hodayfa000-h@users.noreply.github.com>
2025-08-27 19:08:10 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -e
 
-# Set paths to toolchains
-export CROSS_COMPILE=$(pwd)/tools/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CC=$(pwd)/tools/clang/host/linux-x86/clang-r407598/bin/clang
+# Set up toolchain paths
+export CROSS_COMPILE=$(pwd)/gcc/bin/aarch64-linux-android-
+export CC=$(pwd)/clang/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=r
 
-# Optional flags to suppress warnings
+# Optional flags
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
