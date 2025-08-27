ca8a1d3264ea63ab8311c834c1e67b2f618b93c5
hodayfa000h <127386194+hodayfa000-h@users.noreply.github.com>
2025-08-27 19:03:38 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/../aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/../clang-r383902/bin/clang
+# Set paths to toolchains
+export CROSS_COMPILE=$(pwd)/tools/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CC=$(pwd)/tools/clang/host/linux-x86/clang-r407598/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=r
 
+# Optional flags to suppress warnings
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
+# Build kernel
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc --all) a32_defconfig
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc --all)
 
+# Copy final Image
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
